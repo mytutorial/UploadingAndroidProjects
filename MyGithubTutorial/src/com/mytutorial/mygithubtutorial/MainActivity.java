3e39e1ad4b41366ae5c5fbdeacb923b95f0a8a54
harrylingad <harry_lingad2011@yahoo.com>
2016-02-20 15:39:05 +0800

    Uploading project

---
+++ MyGithubTutorial/src/com/mytutorial/mygithubtutorial/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mytutorial.mygithubtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_activity);
+	}
+	
+	
+
+}
